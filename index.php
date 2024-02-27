721af1ace882220f3f123a33f084b27091dfca47
slyriyad <156778836+slyriyad@users.noreply.github.com>
2024-02-27 10:32:12 +0100

    replace css with  bootstrap only

--- index.php
+++ index.php
@@ -8,71 +8,72 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/> -->
     <title>Ajout produit</title>
 </head>
 <body>
-    <div id="wrapper">
-        <header class="indexHeader">
+    <div id="wrapper" class="h-100 d-flex flex-column justify-content-center align-items-center">
+        <header class="display-3" style="color: rgb(0, 123, 255); margin-bottom: 50px; line-height: 105px; font-weight: 600;"">
             LE MARCHÉ.
         </header>
         <main>
-        <div class="button">
-            <button class="btn btn-primary" type="submit">Ajouter produit</button>
-            <a type="button" class="btn btn-light position-relative" href="http://localhost/Riyad_FOUZI/appli/recap.php">
-            Panier
-            </a>    
-            <span class="position-absolute top-10 start-60 translate-middle badge rounded-pill bg-danger">
-            <?php
-            if (isset($_SESSION['products'])) {
-                $totalProduct = 0;
-                foreach($_SESSION['products'] as $index => $product) { // Sinon calcul le nombre de produit
-                    $totalProduct += $product['qtt'];
-            }}
-            echo $totalProduct;
-            ?>
-                <span class="visually-hidden">unread messages</span>
-            </span>
-        </button>
-        </div>  
-            <h1>Ajouter un produit</h1>
-            <form action="traitement.php?action=add" method="post">
-                <p>
-                    <label>
-                        Nom du produit :
-                        <input class="form-control" type="text" placeholder="Nom" name="name">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Prix du produit en € :
-                        <input class="form-control" step="any" type="number" placeholder="Prix" name="price">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Quantité désirée :
-                        <input class="form-control" type="number" name="qtt" value="1" >
-                    </label>
-                </p>
-                <button name ="submit" type="submit" class="btn btn-primary" id="liveAlertBtn" >Ajouter le produit</button>
-            </form>
-            
+        <div class="h-100 d-flex flex-column justify-content-center align-items-center">
+            <div class="button">
+                <button class="btn btn-primary" type="submit">Ajouter produit</button>
+                <a type="button" class="btn btn-light position-relative" href="recap.php">
+                Panier
+                </a>    
+                <span class="position-absolute top-10 start-60 translate-middle badge rounded-pill bg-danger">
                 <?php
-                    // Affiche les messages de succès ou d'erreur
-                    if (isset($_SESSION['success_message'])) {
-                        
-                        echo $_SESSION['success_message'] ;
-                        
-                        unset($_SESSION['success_message']);
-                       
-                    } elseif (isset($_SESSION['error_message'])){ 
-                        
-                        echo $_SESSION['error_message'] ;
-                        unset($_SESSION['error_message']);
-
-                    }
+                $totalProduct = 0;
+                if (isset($_SESSION['products'])) {
+                    foreach($_SESSION['products'] as $index => $product) { // Sinon calcul le nombre de produit
+                        $totalProduct += $product['qtt'];
+                    }}
+                    echo $totalProduct;
                 ?>
+                    <span class="visually-hidden">unread messages</span>
+                </span>
+            </button>
+            </div>  
+                <h1>Ajouter un produit</h1>
+                <form action="traitement.php?action=add" method="post">
+                    <p>
+                        <label>
+                            Nom du produit :
+                            <input class="form-control" type="text" placeholder="Nom" name="name">
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Prix du produit en € :
+                            <input class="form-control" step="any" type="number" placeholder="Prix" name="price">
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Quantité désirée :
+                            <input class="form-control" type="number" name="qtt" value="1" >
+                        </label>
+                    </p>
+                    <button name ="submit" type="submit" class="btn btn-primary" id="liveAlertBtn" >Ajouter le produit</button>
+                </form>
+                
+                    <?php
+                        // Affiche les messages de succès ou d'erreur
+                        if (isset($_SESSION['success_message'])) {
+                            
+                            echo $_SESSION['success_message'] ;
+                            
+                            unset($_SESSION['success_message']);
+                        
+                        } elseif (isset($_SESSION['error_message'])){ 
+                            
+                            echo $_SESSION['error_message'] ;
+                            unset($_SESSION['error_message']);
+
+                        }
+                    ?>
         </div>
         </main>
         <script src="main.js"></script>
--- recap.php
+++ recap.php
@@ -7,28 +7,28 @@ session_start();
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"/> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Récapitulatif des produits</title>
 </head>
 <body class="recapBody">
-    <div id="wrapper">
-    <header class="recapHeader">
+    <div id="wrapper" class="h-100 d-flex flex-column justify-content-center align-items-center">
+    <header class="display-3" style="color: rgb(0, 123, 255); margin-bottom: 50px; line-height: 105px; font-weight: 600;" >
         LE MARCHÉ.
     </header>
     <div id="btnRecap">
-        <a type="button" class="btn btn-light position-relative" href="http://localhost/Riyad_FOUZI/appli/index.php">
+        <a type="button" class="btn btn-light position-relative" href="index.php">
             Ajouter produit
         </a>
-        <a type="button" class="btn btn-primary position-relative" href="http://localhost/Riyad_FOUZI/appli/recap.php">
+        <a type="button" class="btn btn-primary position-relative" href="recap.php">
             Panier
         </a>    
         <span id="badge" class="position-absolute top-10 start-60  translate-middle badge rounded-pill bg-danger">
             <?php
-            if (isset($_SESSION['products'])) {
                 $totalProduct = 0;
+            if (isset($_SESSION['products'])) {
                 foreach($_SESSION['products'] as $index => $product) { // Sinon calcul le nombre de produit
                     $totalProduct += $product['qtt'];
             }}
